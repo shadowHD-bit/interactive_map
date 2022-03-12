55e65f536628960c4c43a89fdaf7166132b134ae
Владислав Медяннкин <medyankin03@mail.ru>
2022-03-12 23:14:21 +0700

    Добавлен Герб

--- map.js
+++ map.js
@@ -5,8 +5,8 @@ let placesGroup = [
         style: "islands#blueIcon",
         color: 'blue',
         places: [
-            {coordinates: [56.478094, 84.950290], adress: 'Пр.Ленина 85а', info:'Decs1', name: 'Мэйк лав пицца'},
-            {coordinates: [56.483895, 84.982243], adress: 'Комсомольский проспект 14а', info:'Decs1', name: 'Мэйк лав пицца'}
+            {coordinates: [56.478094, 84.950290], adress: 'Проспект Ленина, 85а', info:'Decs1', name: 'Make Love Pizza'},
+            {coordinates: [56.483895, 84.982243], adress: 'Комсомольский проспект, 14а', info:'Decs1', name: 'Make Love Pizza'}
         ]
     },
     {
@@ -14,9 +14,8 @@ let placesGroup = [
         style: "islands#redIcon",
         color: 'red',
         places: [
-            {coordinates: [56.488875, 84.952742], adress: 'Бакунина 3', info:'Decs1', name: 'Воскресенская гора/Музей истории г. Томск'},
-            {coordinates: [56.472466, 84.954745], adress: 'Герцена 6', info:'Decs1', name: 'Музей истории'},
- 		 {coordinates: [56.482662, 84.947406], adress: '​переулок Нахановича 3', info:'Decs1', name: 'Художественный музей'},
+            {coordinates: [56.488875, 84.952742], adress: 'Улица Бакунина, 3', info:'Decs1', name: 'Музей истории города Томск'},
+ 		    {coordinates: [56.482662, 84.947406], adress: '​Переулок Нахановича, 3', info:'Decs1', name: 'Томский областной художественный музей'},
  
         ]
     },
@@ -25,15 +24,12 @@ let placesGroup = [
         style: "islands#greenIcon",
         color: 'green',
         places: [
-            {coordinates: [56.462471, 84.942681], adress: 'ул. Аркадия Иванова, 18А', info:'Decs1', name: 'Аллея Пивоваров'},
-            {coordinates: [56.493919, 84.954647], adress: 'ул. Октябрьский Взвоз, 10', info:'Decs1', name: 'Воскресенская церковь'}
+            {coordinates: [56.462471, 84.942681], adress: 'Улица Аркадия Иванова, 18А', info:'Decs1', name: 'Аллея Пивоваров'},
+            {coordinates: [56.493919, 84.954647], adress: 'Улица Октябрьский Взвоз, 10', info:'Decs1', name: 'Воскресенская церковь'}
         ]
     }
 
-
 ];
- 
- 
  
  ymaps.ready(init);
     function init(){
@@ -45,8 +41,8 @@ let placesGroup = [
         }, { 
             //Границы карты
             restrictMapArea: [
-                [56.598443, 84.708696],
-                [56.449291, 85.113131]
+                [56.578572, 84.807180],
+                [56.396909, 85.165454]
             ]
         }),
 
@@ -116,7 +112,6 @@ let placesGroup = [
                 }
                 return false;
 
-                
             });
     }
 
