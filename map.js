bb3e89d05ae404e96e69a4611bf3682cabb3edda
Владислав Медяннкин <medyankin03@mail.ru>
2022-03-14 20:27:41 +0700

    add new markers

--- map.js
+++ map.js
@@ -6,7 +6,38 @@ let placesGroup = [
         color: 'blue',
         places: [
             {coordinates: [56.478094, 84.950290], adress: 'Проспект Ленина, 85а', info:'Decs1', name: 'Make Love Pizza'},
-            {coordinates: [56.483895, 84.982243], adress: 'Комсомольский проспект, 14а', info:'Decs1', name: 'Make Love Pizza'}
+            {coordinates: [56.483895, 84.982243], adress: 'Комсомольский проспект, 14а', info:'Decs1', name: 'Make Love Pizza'},
+            {coordinates: [56.464510, 84.984812], adress: 'Проспект Кирова, 58', info:'Decs1', name: 'Мир'},
+            {coordinates: [56.491767, 84.970295], adress: 'Улица Яковлева, 42', info:'Decs1', name: 'ОШ'},
+            {coordinates: [56.492875, 84.951287], adress: 'Улица Розы Люксембург, 19', info:'Decs1', name: 'ОШ'},
+            {coordinates: [56.456756,84.952059], adress: 'Проспект Ленина, 1, стр. 1', info:'Decs1', name: 'ОШ'},
+            {coordinates: [56.460497,84.945924], adress: 'Улица Тимакова 19А', info:'Decs1', name: 'ОШ'},
+            {coordinates: [56.467499,84.964007], adress: 'Улица Вершинина, 30', info:'Decs1', name: 'ОШ'},
+            {coordinates: [56.458746, 84.962255], adress: 'Улица Учебная, 37Б/1', info:'Decs1', name: 'Ростовская шаурма'},
+            {coordinates: [56.503364, 85.012597], adress: 'Улица Иркутский тракт, 37', info:'Decs1', name: 'Ростовская шаурма'},
+            {coordinates: [56.460899, 84.964546], adress: 'Улица Вершинина, 46/4', info:'Decs1', name: 'Ням-ням'},
+            {coordinates: [56.502614, 85.003964], adress: 'Улица Иркутский тракт, 12', info:'Decs1', name: 'Бублик'},
+            {coordinates: [56.459218, 84.951556], adress: 'Улица Учебная, 20', info:'Decs1', name: 'Мир суши'},
+            {coordinates: [56.493427, 84.949760], adress: 'Переулок 1905 года, 11', info:'Decs1', name: 'Мир суши'},
+            {coordinates: [56.476543, 84.967106], adress: 'Проспект Фрунзе, 86', info:'Decs1', name: 'Панда'},
+            {coordinates: [56.458452, 84.973035], adress: 'Улица Красноармейская, 128', info:'Decs1', name: 'Панда'},
+            {coordinates: [56.462526, 84.965983], adress: 'Улица Красноармейская, 101А', info:'Decs1', name: 'ШашлыкоFF'},
+            {coordinates: [56.475563, 84.986474], adress: 'Проспект Фрунзе, 103 Д', info:'Decs1', name: 'ШашлыкоFF'},
+            {coordinates: [56.469110, 84.951404], adress: 'Проспект Ленина, 51А', info:'Decs1', name: 'KFC'},
+            {coordinates: [56.456945, 84.960728], adress: 'Улица Нахимова, 8с13', info:'Decs1', name: 'KFC'},
+            {coordinates: [56.488522, 84.978551], adress: 'Комсомольский проспект, 13Б', info:'Decs1', name: 'KFC'},
+            {coordinates: [56.455856, 84.973736], adress: 'Улица Красноармейская, 123А', info:'Decs1', name: 'Макдональдс'},
+            {coordinates: [56.488522, 84.978551], adress: 'Комсомольский проспект, 13Б', info:'Decs1', name: 'Макдональдс'},
+            {coordinates: [56.475852, 84.972667], adress: 'Проспект Фрунзе, 61', info:'Decs1', name: 'Буланже'},
+            {coordinates: [56.493084, 84.949113], adress: 'Проспект Ленина, 133', info:'Decs1', name: 'Буланже'},
+            {coordinates: [56.458681, 84.955769], adress: 'Улица Учебная, 35Г', info:'Decs1', name: 'Буланжери'},
+            {coordinates: [56.464336, 84.954260], adress: 'Улица Усова 4', info:'Decs1', name: 'Зеленая улица'},
+            {coordinates: [56.461059, 84.950451], adress: 'Проспект Ленина, 22', info:'Decs1', name: 'Дыхание ВОКА'},
+            {coordinates: [56.453936, 84.975982], adress: 'Улица Красноармейская, 135', info:'Decs1', name: 'А мы тут всё плюшками балуемся'},
+            {coordinates: [56.454717, 84.973376], adress: 'Улица Нахимова, 15', info:'Decs1', name: 'Сибирские блины'},
+            {coordinates: [56.451374, 84.968076], adress: 'Проезд Вершинина, 8', info:'Decs1', name: 'Сибирские блины'},
+            {coordinates: [56.463461, 84.952095], adress: 'Улица Усова, 1', info:'Decs1', name: 'Сибирские блины'},
+            {coordinates: [56.455602, 84.951871], adress: 'Проспект Ленина, 1В', info:'Decs1', name: 'Мама я дома'}
         ]
     },
     {
@@ -16,7 +47,12 @@ let placesGroup = [
         places: [
             {coordinates: [56.488875, 84.952742], adress: 'Улица Бакунина, 3', info:'Decs1', name: 'Музей истории города Томск'},
  		    {coordinates: [56.482662, 84.947406], adress: '​Переулок Нахановича, 3', info:'Decs1', name: 'Томский областной художественный музей'},
- 
+            {coordinates: [56.483164, 84.9464], adress: '​Переулок Нахановича, 4', info:'Decs1', name: 'Театр юного зрителя'},
+            {coordinates: [56.486842, 84.949966], adress: '​Площадь Ленина, 12А', info:'Decs1', name: 'Томская областная филармония'},
+            {coordinates: [56.487573, 84.947334], adress: '​Площадь Ленина, 4', info:'Decs1', name: 'Театр драмы'},
+            {coordinates: [56.475882, 84.949805], adress: '​Проспект Ленина, 44', info:'Decs1', name: 'Музей следственная тюрьма НКВД'},
+            {coordinates: [56.482662, 84.948448], adress: 'Проспект Ленина, 78', info:'Decs1', name: 'Зрелищный центр Аэлита'},
+            {coordinates: [56.454115038934084,84.99614291661064], adress: '​Ботанический переулок', info:'Decs1', name: 'Ботанический сад'},
         ]
     },
     {
@@ -26,6 +62,14 @@ let placesGroup = [
         places: [
             {coordinates: [56.462471, 84.942681], adress: 'Улица Аркадия Иванова, 18А', info:'Decs1', name: 'Аллея Пивоваров'},
             {coordinates: [56.493919, 84.954647], adress: 'Улица Октябрьский Взвоз, 10', info:'Decs1', name: 'Воскресенская церковь'}
+        ]
+    },
+    {
+        category: 'Развлечения',
+        style: "islands#yellowIcon",
+        color: 'green',
+        places: [
+            {coordinates: [56.478526,84.949508], adress: 'Проспект Ленина, 54А', info:'Decs1', name: 'Компьютерный клуб Colezium'},
         ]
     }
 
